59a08ea687b961bbdf82c6ae6c6aba9cc39b3ec6
Juan Manuel Servera <jmservera@outlook.com>
2014-10-17 08:46:46 +0200

    First solution

---
+++ FizzBuzzKata1/FizzBuzz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzKata1
+{
+    public class FizzBuzz
+    {
+        const int fizzBuzz = 15;
+        const int Fizz = 3;
+        const int Buzz = 5;
+        public string Answer(int p)
+        {
+
+            if (isDivisibleBy(p, fizzBuzz))
+            {
+                return "FizzBuzz";
+            }
+            if (isDivisibleBy(p, Fizz))
+            {
+                return "Fizz";
+            }
+            if (isDivisibleBy(p, Buzz))
+            {
+                return "Buzz";
+            }
+            return p.ToString();
+        }
+
+        private static bool isDivisibleBy(int p, int j)
+        {
+            return p % j == 0;
+        }
+       
+        
+    }
+}
---
+++ FizzBuzzKata1/FizzBuzzTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FizzBuzzKata1
+{
+    public class FizzBuzzTest
+    {
+        FizzBuzz _fizzBuzz = new FizzBuzz();
+        //TODO: 
+        // 1 -- 1
+        // 2 -- 2
+        // 3 -- fizz
+        // 4 -- 4
+        // 5 -- buzz
+        //... 15 -- fizzbuzz
+
+        [Fact]
+        public void AnswerShoulReturnOneWhenOne()
+        {
+            Assert.Equal("1", _fizzBuzz.Answer(1));
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(7)]
+        public void Answer_Should_Return_Number_When_Not_Divisible_By3or5(int n)
+        {
+            Assert.Equal(n.ToString(), _fizzBuzz.Answer(n));
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(6)]
+        [InlineData(9)]
+        public void Answer_Should_Return_Fizz_When_Divisible_By3(int n)
+        {
+            Assert.Equal("Fizz", _fizzBuzz.Answer(n));
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(20)]
+        public void Answer_Should_Return_Fizz_When_Divisible_By5(int n)
+        {
+            Assert.Equal("Buzz", _fizzBuzz.Answer(n));
+        }
+
+        [Theory]
+        [InlineData(15)]
+        [InlineData(30)]
+        public void Answer_Should_Return_Fizz_When_Divisible_By15(int n)
+        {
+            Assert.Equal("FizzBuzz", _fizzBuzz.Answer(n));
+        }
+    }
+}
